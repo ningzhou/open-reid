4e73b787bb3d378f31e562bca6419cadf3d3f4bb
Ning Zhou <niz@microsoft.com>
2017-07-06 11:51:29 -0700

    read feature files

--- reid/feature_extraction/__init__.py
+++ reid/feature_extraction/__init__.py
@@ -1,9 +1,11 @@
 from __future__ import absolute_import
 
 from .cnn import extract_cnn_feature
+from .cnn import read_feature_from_disk
 from .database import FeatureDatabase
 
 __all__ = [
     'extract_cnn_feature',
+    'read_feature_from_disk',
     'FeatureDatabase',
 ]
